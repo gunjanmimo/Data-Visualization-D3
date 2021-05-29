9e66a60a0569718904fdb7eaf28dd74f187bc6b6
gunjanmimo <gunjan.mtbpaul@gmail.com>
2021-05-30 00:25:25 +0530

    updateed readme

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# Data-Visualization-D3
--- src/simle.js
+++ src/simle.js
@@ -14,36 +14,33 @@ const mouthArc = arc()
   .startAngle(Math.PI / 1.5)
   .endAngle(Math.PI * 1.3);
 
+const BackgroundCircle = ({ radius }) => {
+  return (
+    <circle r={radius / 2} fill="#ffb300" stroke="black" strokeWidth="3" />
+  );
+};
+const Eye = ({ eyeOffsetX, eyeOffsetY }) => {
+  return (
+    <circle
+      cx={eyeOffsetX}
+      cy={eyeOffsetY}
+      r={svgHeight / 14}
+      fill="black"
+      stroke="white"
+      strokeWidth="25"
+    />
+  );
+};
 function SimileFace() {
   return (
     <div>
       <h1> This is a simile face using svg</h1>
-
       <svg width={svgWidth} height={svgHeight}>
         <g transform={`translate(${centerX},${centerY})`}>
-          <circle
-            r={centerY / 2}
-            fill="#ffb300"
-            stroke="black"
-            strokeWidth="3"
-          />
+          <BackgroundCircle radius={centerY} />
+          <Eye eyeOffsetX={-eyeOffsetX} eyeOffsetY={-eyeOffsetY} />
+          <Eye eyeOffsetX={eyeOffsetX} eyeOffsetY={-eyeOffsetY} />
 
-          <circle
-            cx={-eyeOffsetX}
-            cy={-eyeOffsetY}
-            r={svgHeight / 14}
-            fill="black"
-            stroke="white"
-            strokeWidth="25"
-          />
-          <circle
-            cx={eyeOffsetX}
-            cy={-eyeOffsetY}
-            r={svgHeight / 14}
-            fill="black"
-            stroke="white"
-            strokeWidth="25"
-          />
           <path d={mouthArc()} />
         </g>
       </svg>
