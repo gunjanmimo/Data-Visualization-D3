120a8f8bae007607bb932da51cda53de0a766d80
gunjanmimo <gunjan.mtbpaul@gmail.com>
2021-05-30 10:19:24 +0530

    mouse follower

--- src/mouseFollower/MouseFollower.js
+++ src/mouseFollower/MouseFollower.js
@@ -1,7 +1,24 @@
+import { useState, useCallback } from "react";
+
+const width = 1060;
+const height = 900;
+
+const initialMousePos = { x: width / 2, y: height / 2 };
+
 const MouseFollower = () => {
+  const [mousePos, setMousePos] = useState(initialMousePos);
+  const handleMouseMove = useCallback(
+    (event) => {
+      const { clientX, clientY } = event;
+      setMousePos({ x: clientX - 5, y: clientY - 5 });
+    },
+    [setMousePos]
+  );
   return (
     <div>
-      <h1>Mouse Follower</h1>
+      <svg width={width} height={height} onMouseMove={handleMouseMove}>
+        <circle cx={mousePos.x} cy={mousePos.y} r={width / 20}></circle>
+      </svg>
     </div>
   );
 };
